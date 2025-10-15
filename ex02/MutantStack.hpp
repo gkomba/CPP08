c414dc38581b76efbe6e5b6e8f137f222e55d626
gkomba <kombagildo@gmail.com>
2025-10-15 04:53:50 -0500

    ready to test

--- ex02/MutantStack.hpp
+++ ex02/MutantStack.hpp
@@ -6,7 +6,7 @@
 /*   By: gildo <gildo@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/14 14:26:33 by gkomba            #+#    #+#             */
-/*   Updated: 2025/10/15 02:26:18 by gildo            ###   ########.fr       */
+/*   Updated: 2025/10/15 04:52:49 by gildo            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,21 +23,19 @@ template <class T,  typename Container = std::deque<T> >
 class MutantStack : public std::stack<T, Container>
 {
     public:
-    // typedef std::stack<T> stack;
-    // typedef typename stack::container_type container;
-    typedef typename Container::iterator iterator;
-    typedef typename Container::const_iterator const_iterator;
+        typedef typename Container::iterator iterator;
+        typedef typename Container::const_iterator const_iterator;
     
     MutantStack();
     MutantStack(const MutantStack &obj);
     MutantStack &operator=(const MutantStack &obj);
     ~MutantStack();
-    
     iterator begin();
     iterator end();
     const_iterator begin() const;
     const_iterator end() const;
 };
+
 #include "MutantStack.tpp"
 
 
